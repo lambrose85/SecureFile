1b29a9c804280814a1e9df096b43b33a1014fbbe
Lucas Ambrose <lucasambrose@siu.edu>
2019-04-08 21:12:49 -0500

    Added class that allows access to filesystem and returns absolute path to selected file. Also changed name of File access to login.

--- src/Login.java
+++ src/Login.java
@@ -12,7 +12,7 @@ import javax.swing.SwingConstants;
 import javax.swing.JButton;
 import com.jgoodies.forms.factories.DefaultComponentFactory;
 
-public class FileAccess extends JFrame {
+public class Login extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textField;
@@ -25,7 +25,7 @@ public class FileAccess extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					FileAccess frame = new FileAccess();
+					Login frame = new Login();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -37,7 +37,7 @@ public class FileAccess extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public FileAccess() {
+	public Login() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
